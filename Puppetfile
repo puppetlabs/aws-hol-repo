66a29788742921e7766e31e80a462a8ffb24e6be
Cody Herriges <193064+ody@users.noreply.github.com>
2021-06-14 22:31:17 +0000

    Add saz-sudo for use is hol example

--- Puppetfile
+++ Puppetfile
@@ -3,3 +3,7 @@ forge 'https://forge.puppet.com'
 mod 'puppetlabs-relay', '2.2.0'
 mod 'puppetlabs-stdlib', '7.1.0'
 mod 'puppetlabs-inifile', '5.0.1'
+
+mod 'saz-sudo',
+  git: 'https://github.com/saz/puppet-sudo.git',
+  ref: 'c7441142a24495575b7dc45373150dff798f9a4d'
--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,1 +1,13 @@
-class profile::base() { }
+class profile::base() {
+
+  include 'sudo'
+
+  sudo::conf { 'centos':
+    priority => 10,
+    content  => 'centos ALL=(ALL) NOPASSWD:ALL'
+  }
+  sudo::conf { 'puppetadmin':
+    priority => 10,
+    content  => 'puppetadmin ALL=(ALL) NOPASSWD:ALL'
+  }
+}
